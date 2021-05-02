0f927167832e700e36eaa38714e3409934981d07
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-05-02 16:46:24 +0900

    Create ElectronicsShop.cpp

---
+++ ElectronicsShop.cpp
@@ -0,0 +1,19 @@
+int getMoneySpent(vector<int> keyboards, vector<int> drives, int b)
+{
+    int sum_max = -1;
+    int sum_check = 0;
+
+    for (size_t i = 0; i < keyboards.size(); ++i)
+    {
+        for (size_t j = 0; j < drives.size(); ++j)
+        {
+            sum_check = keyboards[i] + drives[j];
+            if (sum_check > sum_max && sum_check <= b)
+            {
+                sum_max = sum_check;
+            }
+        }
+    }
+    
+    return sum_max;
+}
